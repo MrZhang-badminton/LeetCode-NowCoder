296602fc71d87992ef2088f3c949254dfa2bd744
zhanghua <964552300@qq.com>
2021-02-28 15:12:29 +0800

    lc25

---
+++ src/oj/lc/q500/lc25/Solution.java
@@ -0,0 +1,44 @@
+package oj.lc.q500.lc25;
+
+import com.sun.xml.internal.ws.api.model.CheckedException;
+import utils.object.ListNode;
+
+import java.util.List;
+
+/**
+ * 每K个翻转链表
+ *
+ * @date 2021年02月28日14:12:04
+ */
+public class Solution {
+	public static ListNode reverseKGroup(ListNode head, int k) {
+		ListNode pHead = new ListNode(-1);
+		ListNode p = pHead;
+		pHead.next = head;
+		int count = k;
+		while (p.next != null && count > 0) {
+			p = p.next;
+			count--;
+		}
+
+		if (count > 0) {
+			return head;
+		}
+
+		pHead.next = reverseKGroup(p.next, k);
+		p.next = null;
+
+		while (head != null) {
+			ListNode temp = head.next;
+			head.next = pHead.next;
+			pHead.next = head;
+			head = temp;
+		}
+		return pHead.next;
+	}
+
+	public static void main(String[] args) {
+		ListNode head = ListNode.getList(new int[]{1, 2, 3, 4, 5});
+		ListNode.printListNode(reverseKGroup(head, 2));
+	}
+}
