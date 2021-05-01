260b272f943b2a34e82459637bf6ac4338a2b571
zhanghua <964552300@qq.com>
2021-05-01 23:02:42 +0800

    1. 更新工具类，增加打印二维数组 2. lc57

---
+++ src/oj/lc/q500/lc57/Solution.java
@@ -0,0 +1,60 @@
+package oj.lc.q500.lc57;
+
+import utils.methods.ArrayUtils;
+
+import java.util.*;
+
+/**
+ * 合并区间
+ *
+ * @date 2021年05月01日22:11:51
+ */
+public class Solution {
+
+	/**
+	 * 先排序，再处理
+	 * @param intervals
+	 * @return
+	 */
+	public static int[][] merge(int[][] intervals) {
+		int n = intervals.length;
+		Arrays.sort(intervals, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if (o1[0] == o2[0]) {
+					return o1[1] - o2[1];
+				}
+				return o1[0] - o2[0];
+			}
+		});
+		Map<Integer, Integer> map = new LinkedHashMap<>();
+		int left = intervals[0][0], right = intervals[0][1];
+		for (int i = 1; i < n; i++) {
+			int l = intervals[i][0], r = intervals[i][1];
+			if (l <= right) {
+				right = Math.max(right, r);
+			} else if (right < l) {
+				map.put(left, right);
+				left = l;
+				right = r;
+			}
+		}
+		map.put(left,right);
+		int[][] ret = new int[map.size()][2];
+		int index = 0;
+		for(Map.Entry entry : map.entrySet()){
+			ret[index][0] = (Integer) entry.getKey();
+			ret[index][1] = (Integer)entry.getValue();
+			index++;
+		}
+		return ret;
+	}
+
+	public static void main(String[] args) {
+
+		int[][] matrix = merge(new int[][]{
+				{1,4},{4,5}
+		});
+		ArrayUtils.printArray(matrix);
+	}
+}
--- src/utils/methods/ArrayUtils.java
+++ src/utils/methods/ArrayUtils.java
@@ -3,22 +3,38 @@ package utils.methods;
 public class ArrayUtils {
 	/**
 	 * 打印数组
+	 *
 	 * @param nums
 	 */
-	public static void printArray(int[] nums){
-		System.out.print("[");
+	public static void printArray(int[] nums) {
+//		System.out.print("[");
 		for (int i = 0; i < nums.length; i++) {
-			if(i < nums.length - 1){
+			if (i < nums.length - 1) {
 				System.out.print(nums[i] + ", ");
-			}else {
+			} else {
 				System.out.print(nums[i]);
 			}
 		}
-		System.out.print("]");
+//		System.out.print("]");
 		System.out.println();
 	}
 
+	public static void printArray(int[][] nums) {
+		int nr = nums.length,nc = nums[0].length;
+		for (int i = 0; i < nr; i++) {
+			for (int j = 0; j < nc; j++) {
+				if(j == nc-1){
+					System.out.print(nums[i][j]);
+					continue;
+				}
+				System.out.print(nums[i][j] + ", ");
+			}
+			System.out.println();
+		}
+	}
+
 	public static void main(String[] args) {
-		printArray(new int[]{1,2,3,4});
+		printArray(new int[][]{{1, 2}, {3, 4}});
+//		printArray(new int[]{1,2,3,4});
 	}
 }
