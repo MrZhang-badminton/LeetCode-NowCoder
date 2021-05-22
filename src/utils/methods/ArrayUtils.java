b0ee6a89a31e9fd80423dd8540e302d451e75c1d
zhanghua <964552300@qq.com>
2021-05-22 16:42:53 +0800

    1. 调整数组工具类输出格式

--- src/utils/methods/ArrayUtils.java
+++ src/utils/methods/ArrayUtils.java
@@ -7,7 +7,7 @@ public class ArrayUtils {
 	 * @param nums
 	 */
 	public static void printArray(int[] nums) {
-//		System.out.print("[");
+		System.out.print("[");
 		for (int i = 0; i < nums.length; i++) {
 			if (i < nums.length - 1) {
 				System.out.print(nums[i] + ", ");
@@ -15,26 +15,38 @@ public class ArrayUtils {
 				System.out.print(nums[i]);
 			}
 		}
-//		System.out.print("]");
+		System.out.print("]");
 		System.out.println();
 	}
 
 	public static void printArray(int[][] nums) {
-		int nr = nums.length,nc = nums[0].length;
+		System.out.print("[");
+		System.out.println();
+		if (nums == null || nums.length == 0 || nums[0].length == 0) {
+			System.out.print("]");
+			return;
+		}
+		int nr = nums.length, nc = nums[0].length;
 		for (int i = 0; i < nr; i++) {
+			System.out.print(" [");
 			for (int j = 0; j < nc; j++) {
-				if(j == nc-1){
+				if (j == nc - 1) {
 					System.out.print(nums[i][j]);
 					continue;
 				}
 				System.out.print(nums[i][j] + ", ");
 			}
+			System.out.print("]");
+			if(i != nr-1){
+				System.out.print(",");
+			}
 			System.out.println();
 		}
+		System.out.print("]");
 	}
 
 	public static void main(String[] args) {
-		printArray(new int[][]{{1, 2}, {3, 4}});
-//		printArray(new int[]{1,2,3,4});
+		printArray(new int[][]{{1, 2, 4}, {3, 4, 10}, {4, 2, 5}});
+
 	}
 }
