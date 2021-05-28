63938e2e1eb42775a9a19b7026c58025b81642d9
zhanghua <964552300@qq.com>
2021-05-28 17:05:11 +0800

    将两种方法写到一个类中

--- src/oj/lc/q500/lc200/Solution.java
+++ src/oj/lc/q500/lc200/Solution.java
@@ -1,5 +1,8 @@
 package oj.lc.q500.lc200;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
 
 	public static void dfs(char[][] grid, int r, int c) {
@@ -36,6 +39,48 @@ public class Solution {
 		int sum = Integer.valueOf("123") + Integer.valueOf("123");
 		return islandsSums;
 
+	}
+
+	public static int numIslands2(char[][] grid) {
+		if (grid == null) {
+			return 0;
+		}
+		int nr = grid.length;
+		int nc = grid[0].length;
+		int islandSum = 0;
+
+		for (int r = 0; r < nr; r++) {
+			for (int c = 0; c < nc; c++) {
+				if (grid[r][c] == '1') {
+					islandSum++;
+					grid[r][c] = '0';
+					Queue<Integer> queue = new LinkedList<>();
+					queue.add(r * nc + c);
+					while (!queue.isEmpty()) {
+						int id = queue.remove();
+						int row = id / nc;
+						int col = id % nc;
+						if (row - 1 >= 0 && grid[row - 1][col] == '1') {
+							queue.add((row - 1) * nc + col);
+							grid[row - 1][col] = '0';
+						}
+						if (row + 1 < nr && grid[row + 1][col] == '1') {
+							queue.add((row + 1) * nc + col);
+							grid[row + 1][col] = '0';
+						}
+						if (col - 1 >= 0 && grid[row][col - 1] == '1') {
+							queue.add(row * nc + col - 1);
+							grid[row][col - 1] = '0';
+						}
+						if (col + 1 < nc && grid[row][col + 1] == '1') {
+							queue.add(row * nc + col + 1);
+							grid[row][col + 1] = '0';
+						}
+					}
+				}
+			}
+		}
+		return islandSum;
 	}
 
 	public static void main(String[] args) {
