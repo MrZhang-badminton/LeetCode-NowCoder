f3198f62d90451106e43d7098cdb4b010a943837
zhanghua <964552300@qq.com>
2021-06-08 15:03:29 +0800

    lc543

---
+++ src/oj/lc/q1000/lc543/Solution.java
@@ -0,0 +1,35 @@
+package oj.lc.q1000.lc543;
+
+import utils.ds.TreeNode;
+
+public class Solution {
+
+	private static int width;
+
+	public static int diameterOfBinaryTree(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		width = 0;
+		getHeight(root);
+		return width;
+	}
+
+	public static int getHeight(TreeNode root) {
+		if(root == null){
+			return 0;
+		}
+		int left = getHeight(root.left);
+		int right = getHeight(root.right);
+
+		width = Math.max(width,left+right);
+		return Math.max(left,right) + 1;
+	}
+
+
+
+	public static void main(String[] args) {
+		TreeNode root = TreeNode.createTree(new int[]{1, 2, 3, 4, 5});
+		System.out.println(diameterOfBinaryTree(root));
+	}
+}
