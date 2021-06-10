9d3748699c3d43f3c56ba7c9582c61417bf94ce8
zhanghua <964552300@qq.com>
2021-06-10 13:31:19 +0800

    lc142

---
+++ src/oj/lc/q500/lc142/Solution.java
@@ -0,0 +1,48 @@
+package oj.lc.q500.lc142;
+
+import utils.ds.ListNode;
+
+public class Solution {
+
+
+	/**
+	 * 双指针
+	 *
+	 * @param head
+	 * @return
+	 */
+	public static ListNode detectCycle2(ListNode head) {
+		if (head == null) {
+			return null;
+		}
+
+		ListNode slow = head, fast = head;
+		while (fast != null) {
+			slow = slow.next;
+			fast = fast.next;
+			if (fast != null) {
+				fast = fast.next;
+			}
+			if (fast == null) {
+				return null;
+			}
+			if (fast != null && fast == slow) {
+				fast = head;
+				while (fast != slow) {
+					slow = slow.next;
+					fast = fast.next;
+
+				}
+				return slow;
+			}
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+		ListNode head = ListNode.createList(new int[]{3, 2, 0, -4});
+		head.next.next.next.next = head.next;
+		System.out.println(detectCycle2(head).val);
+	}
+}
+
