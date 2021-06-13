c556a042252df23184bed49ffd20791e2b104961
zhanghua <964552300@qq.com>
2021-06-13 23:53:21 +0800

    lc210

---
+++ src/oj/lc/q500/lc210/Solution.java
@@ -0,0 +1,71 @@
+package oj.lc.q500.lc210;
+
+import utils.methods.ArrayUtils;
+
+import java.util.*;
+
+public class Solution {
+
+	/**
+	 * 拓扑排序
+	 *
+	 * @param numCourses
+	 * @param prerequisites
+	 * @return
+	 */
+	public static int[] findOrder(int numCourses, int[][] prerequisites) {
+		int[] result = new int[numCourses];
+		int[] in = new int[numCourses];
+		int n = prerequisites.length;
+		int count = 0;
+
+		List<List<Integer>> edgesList = new ArrayList<>();
+		Queue<Integer> queue = new LinkedList<>();
+
+		for (int i = 0; i < numCourses; i++) {
+			edgesList.add(new ArrayList<>());
+		}
+
+		for (int i = 0; i < n; i++) {
+			in[prerequisites[i][0]]++;
+			List<Integer> list = edgesList.get(prerequisites[i][1]);
+			list.add(prerequisites[i][0]);
+		}
+
+		for (int i = 0; i < numCourses; i++) {
+			if (in[i] == 0) {
+				queue.offer(i);
+			}
+		}
+
+		while (!queue.isEmpty()) {
+			int num = queue.poll();
+			result[count++] = num;
+			//更新边
+			List<Integer> tList = edgesList.get(num);
+			for (int k : tList) {
+				in[k]--;
+				if (in[k] == 0) {
+					queue.offer(k);
+				}
+			}
+		}
+		if (count < numCourses) {
+			return new int[0];
+		}
+		return result;
+	}
+
+
+	public static void main(String[] args) {
+		int[] result = findOrder(3, new int[][]{
+				{1, 0},
+				{0, 1},
+//				{1, 2}
+//				{2, 0},
+//				{3, 1},
+//				{3, 2}
+		});
+		ArrayUtils.printArray(result);
+	}
+}
---
+++ src/oj/lc/q500/lc210/Solution2.java
@@ -0,0 +1,76 @@
+package oj.lc.q500.lc210;
+
+import utils.methods.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution2 {
+
+	static List<List<Integer>> edgeList = new ArrayList<>();
+	static int[] visited;
+	static int[] result;
+	static boolean valid;
+	static int index;
+
+	/**
+	 * 深度优先遍历
+	 *
+	 * @param numCourses
+	 * @param prerequisites
+	 * @return
+	 */
+	public static int[] findOrder(int numCourses, int[][] prerequisites) {
+		edgeList = new ArrayList<>();
+		for (int i = 0; i < numCourses; i++) {
+			edgeList.add(new ArrayList<>());
+		}
+		visited = new int[numCourses];
+		result = new int[numCourses];
+		index = numCourses - 1;
+		valid = true;
+		for (int[] info : prerequisites) {
+			edgeList.get(info[1]).add(info[0]);
+		}
+		for (int i = 0; i < numCourses && valid; i++) {
+			if (visited[i] == 0) {
+				dfs(i);
+			}
+		}
+		if (!valid) {
+			return new int[0];
+		}
+		return result;
+	}
+
+	public static void dfs(int u) {
+		visited[u] = 1;
+		for (int v : edgeList.get(u)) {
+
+			if (visited[v] == 0) {
+				dfs(v);
+				if (!valid) {
+					return;
+				}
+			} else if (visited[v] == 1) {
+				valid = false;
+				return;
+			}
+		}
+		visited[u] = 2;
+		result[index--] = u;
+	}
+
+
+	public static void main(String[] args) {
+		int[] result = findOrder(2, new int[][]{
+				{1, 0},
+//				{0, 1},
+//				{1, 2}
+//				{2, 0},
+//				{3, 1},
+//				{3, 2}
+		});
+		ArrayUtils.printArray(result);
+	}
+}
