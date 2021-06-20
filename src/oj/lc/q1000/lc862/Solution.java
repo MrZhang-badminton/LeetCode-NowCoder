015699d590b863918ca34016d9b917401fb58fea
zhanghua <964552300@qq.com>
2021-06-20 16:14:03 +0800

    更新lc862

--- src/oj/lc/q1000/lc862/Solution.java
+++ src/oj/lc/q1000/lc862/Solution.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 
 public class Solution {
 
-	public static int shortestSubarray2(int[] nums, int k) {
+	public static int shortestSubarray(int[] nums, int k) {
 		int n = nums.length;
 		int right = 0;
 		int sum = 0, minLen = Integer.MAX_VALUE;
@@ -29,26 +29,35 @@ public class Solution {
 		return minLen == Integer.MAX_VALUE ? -1 : minLen;
 	}
 
-	public static int shortestSubarray(int[] nums, int k) {
+	/**
+	 * 前缀和 + 滑动窗口
+	 *
+	 * @param nums
+	 * @param k
+	 * @return
+	 */
+	public static int shortestSubarray2(int[] nums, int k) {
 		int n = nums.length;
-		long[] p = new long[n + 1];
-		for (int i = 0; i < n; i++) {
-			p[i + 1] = p[i] + (long) nums[i];
-		}
-		int ans = n + 1;
+		long[] arr = new long[n + 1];
+		int max = Integer.MAX_VALUE;
 		Deque<Integer> deque = new LinkedList<>();
 
-		for (int y = 0; y < p.length; y++) {
-			while (!deque.isEmpty() && p[y] <= p[deque.getLast()]) {
+		for (int i = 0; i < n; i++) {
+			arr[i + 1] = arr[i] + (long) nums[i];
+		}
+
+		for (int i = 0; i < n + 1; i++) {
+			while (!deque.isEmpty() && arr[deque.getLast()] >= arr[i]) {
 				deque.removeLast();
 			}
-			while (!deque.isEmpty() && p[y] >= p[deque.getFirst()] + k) {
-				ans = Math.min(ans, y - deque.removeFirst());
+			while (!deque.isEmpty() && arr[i] - arr[deque.getFirst()] >= k) {
+				max = Math.min(max, i - deque.removeFirst());
 			}
-			deque.addLast(y);
+			deque.addLast(i);
 		}
-		return ans < n + 1 ? ans : -1;
+		return max == Integer.MAX_VALUE ? -1 : max;
 	}
+
 
 	public static void main(String[] args) {
 		System.out.println(shortestSubarray(new int[]{
