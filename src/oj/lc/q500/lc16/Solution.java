00116d4999a3705aea87712972d622068bcfa63a
zhanghua <964552300@qq.com>
2021-08-13 02:26:35 +0800

    lc16

--- src/oj/lc/q500/lc16/Solution.java
+++ src/oj/lc/q500/lc16/Solution.java
@@ -5,39 +5,43 @@ import java.util.Arrays;
 public class Solution {
 
 	public static int threeSumClosest(int[] nums, int target) {
-		int n = nums.length;
 		Arrays.sort(nums);
-		int ans = Integer.MAX_VALUE;
+		int n = nums.length;
+		//这边不能设置为Integer.MAX_VALUE
+		//因为当Math.abs(Integer.MAX_VALUE - (-1))的时候发生溢出
+		//int result = Integer.MAX_VALUE;
+
+		//这边设置为16进制0x3f3f3f3f,既可以表示很大的数,又可以在大多数时候避免溢出
+		int result = 0x3f3f3f3f;
 		for (int i = 0; i < n - 2; i++) {
-			if (i > 0 && nums[i] == nums[i - 1]) {
+			if (i != 0 && nums[i] == nums[i - 1]) {
 				continue;
 			}
-
-			int left = i + 1, right = n - 1;
-			while (left < right) {
-				int sum = nums[i] + nums[left] + nums[right];
+			int l = i + 1, r = n - 1;
+			while (l < r) {
+				int sum = nums[i] + nums[l] + nums[r];
 				if (sum == target) {
-					return sum;
+					return target;
 				}
-				if (Math.abs(sum - target) < Math.abs(ans - target)) {
-					ans = sum;
+				if (Math.abs(sum - target) < Math.abs(result - target)) {
+					result = sum;
 				}
-				if (sum < target) {
-					int l = left + 1;
-					while (l < right && nums[l] == nums[l - 1]) {
-						l++;
+				if (sum > target) {
+					int j = r - 1;
+					while (l < j && nums[j] == nums[r]) {
+						j--;
 					}
-					left = l;
+					r = j;
 				} else {
-					int r = right - 1;
-					while (left < r && nums[r] == nums[r + 1]) {
-						r--;
+					int k = l + 1;
+					while (k < r && nums[k] == nums[l]) {
+						k++;
 					}
-					right = r;
+					l = k;
 				}
 			}
 		}
-		return ans;
+		return result;
 	}
 
 	public static void main(String[] args) {
