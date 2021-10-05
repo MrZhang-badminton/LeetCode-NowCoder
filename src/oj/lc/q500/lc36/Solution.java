e15290a02092db6ef7a250b445132454b35481a8
964552300@qq.com <964552300@qq.com>
2021-10-05 21:51:15 +0800

    lc36补充

--- src/oj/lc/q500/lc36/Solution.java
+++ src/oj/lc/q500/lc36/Solution.java
@@ -11,6 +11,11 @@ import java.util.Set;
 public class Solution {
 
 
+	/**
+	 * 简单遍历（写法有点复杂）
+	 * @param board
+	 * @return
+	 */
 	public static boolean isValidSudoku(char[][] board) {
 		if (!judgeRowOrCol(board)) {
 			return false;
@@ -50,6 +55,37 @@ public class Solution {
 					return false;
 				} else if (num2 != '.') {
 					colSet.add(num2);
+				}
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * 来自评论
+	 * @param board
+	 * @return
+	 */
+	public boolean isValidSudoku2(char[][] board) {
+		// 记录某行，某位数字是否已经被摆放
+		boolean[][] row = new boolean[9][9];
+		// 记录某列，某位数字是否已经被摆放
+		boolean[][] col = new boolean[9][9];
+		// 记录某 3x3 宫格内，某位数字是否已经被摆放
+		boolean[][] block = new boolean[9][9];
+
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (board[i][j] != '.') {
+					int num = board[i][j] - '1';
+					int blockIndex = i / 3 * 3 + j / 3;
+					if (row[i][num] || col[j][num] || block[blockIndex][num]) {
+						return false;
+					} else {
+						row[i][num] = true;
+						col[j][num] = true;
+						block[blockIndex][num] = true;
+					}
 				}
 			}
 		}
