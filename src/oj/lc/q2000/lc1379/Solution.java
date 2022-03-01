1498868aa70bfebfbd2075d8ef09eed61c4af5a2
zhanghua <964552300@qq.com>
2022-03-02 00:41:30 +0800

    lc1379

---
+++ src/oj/lc/q2000/lc1379/Solution.java
@@ -0,0 +1,57 @@
+package oj.lc.q2000.lc1379;
+
+import utils.ds.TreeNode;
+
+/**
+ * @Description:
+ * @Author: zhanghua
+ * @Date: 2022/3/2 12:24 上午
+ */
+public class Solution {
+
+	private static TreeNode resultNode;
+	private static boolean flag = true;
+
+	/**
+	 * 这个方法比较粗暴，写的也比较丑
+	 *
+	 * @param original
+	 * @param cloned
+	 * @param target
+	 * @return
+	 */
+	public static final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+		inOrder(original, cloned, target);
+		return resultNode;
+	}
+
+	public static void inOrder(TreeNode original, TreeNode cloned, TreeNode target) {
+		if (original == null) {
+			return;
+		}
+		if (!flag) {
+			return;
+		}
+		inOrder(original.left, cloned.left, target);
+		if (!flag) {
+			return;
+		}
+		if (target == original) {
+			flag = false;
+			resultNode = cloned;
+		}
+		if (!flag) {
+			return;
+		}
+		inOrder(original.right, cloned.right, target);
+
+	}
+
+
+	public static void main(String[] args) {
+		TreeNode original = TreeNode.createTree(new int[]{7, 4, 3, -1, -1, 6, 19});
+		TreeNode cloned = TreeNode.createTree(new int[]{7, 4, 3, -1, -1, 6, 19});
+		TreeNode target = original;
+		System.out.println(getTargetCopy(original, cloned, target));
+	}
+}
---
+++ src/oj/lc/q2000/lc1379/SolutionV2.java
@@ -0,0 +1,33 @@
+package oj.lc.q2000.lc1379;
+
+import utils.ds.TreeNode;
+
+/**
+ * @Description:
+ * @Author: zhanghua
+ * @Date: 2022/3/2 12:38 上午
+ */
+public class SolutionV2 {
+
+	/**
+	 * 来自评论区的做法，确实挺巧妙地
+	 * @param original
+	 * @param cloned
+	 * @param target
+	 * @return
+	 */
+	public static final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+		if (original == null) return null;
+		if (original == target) return cloned;
+		TreeNode left = getTargetCopy(original.left, cloned.left, target);
+		if (left != null) return left;
+		return getTargetCopy(original.right, cloned.right, target);
+	}
+
+	public static void main(String[] args) {
+		TreeNode original = TreeNode.createTree(new int[]{7, 4, 3, -1, -1, 6, 19});
+		TreeNode cloned = TreeNode.createTree(new int[]{7, 4, 3, -1, -1, 6, 19});
+		TreeNode target = original;
+		System.out.println(getTargetCopy(original, cloned, target));
+	}
+}
