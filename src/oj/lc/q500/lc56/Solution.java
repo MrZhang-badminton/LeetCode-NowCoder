c18cefdea69b450e5ad2990c6de83e283dd16e41
zhanghua <964552300@qq.com>
2022-11-04 00:32:06 +0800

    简化写法

--- src/oj/lc/q500/lc56/Solution.java
+++ src/oj/lc/q500/lc56/Solution.java
@@ -3,6 +3,7 @@ package oj.lc.q500.lc56;
 import utils.ArrayUtils;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 合并区间
@@ -11,56 +12,58 @@ import java.util.*;
  */
 public class Solution {
 
+
 	/**
-	 * 先排序，再处理
+	 * 先排序, 再处理
 	 * @param intervals
 	 * @return
 	 */
 	public static int[][] merge(int[][] intervals) {
 		int n = intervals.length;
-		Arrays.sort(intervals, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				if (o1[0] == o2[0]) {
-					return o1[1] - o2[1];
-				}
-				return o1[0] - o2[0];
+		Arrays.sort(intervals, (o1, o2) -> {
+			if (o1[0] == o2[0]) {
+				return o1[1] - o2[1];
 			}
+			return o1[0] - o2[0];
 		});
-		Map<Integer, Integer> map = new LinkedHashMap<>();
+
+		Map<Integer, Integer> map = new HashMap<>();
 		int left = intervals[0][0], right = intervals[0][1];
+
 		for (int i = 1; i < n; i++) {
+
 			int l = intervals[i][0], r = intervals[i][1];
 			if (l <= right) {
 				right = Math.max(right, r);
-			} else if (right < l) {
+			} else {
 				map.put(left, right);
 				left = l;
 				right = r;
 			}
 		}
-		map.put(left,right);
-		int[][] ret = new int[map.size()][2];
-		int index = 0;
-		for(Map.Entry entry : map.entrySet()){
-			ret[index][0] = (Integer) entry.getKey();
-			ret[index][1] = (Integer)entry.getValue();
-			index++;
+
+		map.put(left, right);
+		int size = map.size();
+		int[][] result = new int[size][2];
+		int k = 0;
+		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+			result[k][0] = entry.getKey();
+			result[k++][1] = entry.getValue();
 		}
-		return ret;
+		return result;
 	}
+
 
 	public static void main(String[] args) {
 
 		int[][] matrix = merge(new int[][]{
-				{1,4},{4,5}
+				{1, 4}, {4, 5}
 		});
 		ArrayUtils.printArray(matrix);
 
 
-
-		List<Integer> list = new ArrayList<>();
-		list.add(123);
-		System.out.println(list.get(0));
+//		List<Integer> list = new ArrayList<>();
+//		list.add(123);
+//		System.out.println(list.get(0));
 	}
 }
