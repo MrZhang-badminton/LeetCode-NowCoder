92ea71c0e9e2eb67fd58984385a00b01e000c5f6
zhanghua <964552300@qq.com>
2022-11-04 01:12:42 +0800

    review

--- src/oj/lc/q500/lc239/Solution.java
+++ src/oj/lc/q500/lc239/Solution.java
@@ -2,37 +2,19 @@ package oj.lc.q500.lc239;
 
 import utils.ArrayUtils;
 
-import java.util.Comparator;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Solution {
 
-	public static int[] maxSlidingWindow(int[] nums, int k) {
-		int n = nums.length;
-		PriorityQueue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o1[0] != o2[0] ? o2[0] - o1[0] : o2[1] - o1[1];
-			}
-		});
-		for (int i = 0; i < k; i++) {
-			queue.offer(new int[]{nums[i], i});
-		}
-		int[] ans = new int[n - k + 1];
-		ans[0] = queue.peek()[0];
-		for (int i = k; i < n; i++) {
-			queue.offer(new int[]{nums[i], i});
-			while (queue.peek()[1] < i - k) {
-				queue.poll();
-			}
-			ans[i - k + 1] = queue.peek()[0];
-		}
-		return ans;
-	}
 
-	public static int[] maxSlidingWindow2(int[] nums, int k) {
+	/**
+	 * 滑动窗口最优美写法，来自题解
+	 *
+	 * @param nums
+	 * @param k
+	 * @return
+	 */
+	public static int[] maxSlidingWindow(int[] nums, int k) {
 		int n = nums.length;
 		int[] ans = new int[n - k + 1];
 		Deque<Integer> deque = new LinkedList<>();
@@ -54,6 +36,77 @@ public class Solution {
 
 
 			ans[i - k + 1] = nums[deque.getFirst()];
+		}
+		return ans;
+	}
+
+
+	/**
+	 * 滑动窗口自己写法
+	 *
+	 * @param nums
+	 * @param k
+	 * @return
+	 */
+	public static int[] maxSlidingWindow2(int[] nums, int k) {
+		int n = nums.length;
+		Deque<Integer> deque = new LinkedList<>();
+		List<Integer> list = new ArrayList<>();
+
+		for (int j = 0; j < k - 1; j++) {
+			while (!deque.isEmpty() && nums[deque.peekLast()] < nums[j]) {
+				deque.removeLast();
+			}
+			deque.addLast(j);
+		}
+
+		for (int j = k - 1; j < n; j++) {
+			while (!deque.isEmpty() && deque.peekFirst() < j - k + 1) {
+				deque.removeFirst();
+			}
+			int num = nums[j];
+			if (!deque.isEmpty()) {
+				list.add(Math.max(nums[deque.peekFirst()], num));
+			} else {
+				list.add(num);
+			}
+
+			while (!deque.isEmpty() && nums[deque.peekLast()] < num) {
+				deque.removeLast();
+			}
+			deque.addLast(j);
+
+		}
+
+		int[] res = new int[list.size()];
+		for (int j = 0; j < list.size(); j++) {
+			res[j] = list.get(j);
+		}
+		return res;
+	}
+
+
+	/**
+	 * 优先队列写法
+	 *
+	 * @param nums
+	 * @param k
+	 * @return
+	 */
+	public static int[] maxSlidingWindow3(int[] nums, int k) {
+		int n = nums.length;
+		PriorityQueue<int[]> queue = new PriorityQueue<>((o1, o2) -> o1[0] != o2[0] ? o2[0] - o1[0] : o2[1] - o1[1]);
+		for (int i = 0; i < k; i++) {
+			queue.offer(new int[]{nums[i], i});
+		}
+		int[] ans = new int[n - k + 1];
+		ans[0] = queue.peek()[0];
+		for (int i = k; i < n; i++) {
+			queue.offer(new int[]{nums[i], i});
+			while (queue.peek()[1] < i - k) {
+				queue.poll();
+			}
+			ans[i - k + 1] = queue.peek()[0];
 		}
 		return ans;
 	}
