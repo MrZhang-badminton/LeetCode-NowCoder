34ae9694f689fa9185a705562b577d6a47b583c3
zhanghua <964552300@qq.com>
2022-11-20 02:09:27 +0800

    lc1143

--- src/oj/lc/q2000/lc1143/Solution.java
+++ src/oj/lc/q2000/lc1143/Solution.java
@@ -1,10 +1,39 @@
-package oj.lc.temp;
+package oj.lc.q2000.lc1143;
 
-public class LCS {
+/**
+ * @Description:
+ * @Author: zhanghua
+ * @Date: 2022/11/20 02:07
+ */
+public class Solution {
+
 
 	/**
-	 * 待完成
-	 * 最长公共子序列
+	 * （补充）最长公共子序列, 返回最长子序列字符串版本
+	 *
+	 * @param s1
+	 * @param s2
+	 * @return
+	 */
+	public int longestCommonSubsequence(String s1, String s2) {
+		char[] str1 = s1.toCharArray(), str2 = s2.toCharArray();
+		int[][] dp = new int[str1.length + 1][str2.length + 1];
+
+		for (int i = 1; i <= str1.length; i++) {
+			for (int j = 1; j <= str2.length; j++) {
+				if (str1[i - 1] == str2[j - 1]) {
+					dp[i][j] = dp[i - 1][j - 1] + 1;
+				} else {
+					dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+				}
+			}
+		}
+		return dp[str1.length][str2.length];
+	}
+
+
+	/**
+	 * （补充）最长公共子序列, 返回最长子序列字符串版本
 	 *
 	 * @param s1
 	 * @param s2
@@ -42,13 +71,14 @@ public class LCS {
 		int k = dp[i][j];
 		while (k > 0) {
 			if (flag[i][j] == 0) {
-				sb.append(str1[j-1]);
-				i--;j--;
+				sb.append(str1[j - 1]);
+				i--;
+				j--;
 			} else if (flag[i][j] == 1) {
-				sb.append(str2[i-1]);
+				sb.append(str2[i - 1]);
 				i--;
 			} else {
-				sb.append(str1[j-1]);
+				sb.append(str1[j - 1]);
 				j--;
 			}
 			k--;
